c30992ec32d2ca21a2edf1b34f1658a5e627e0a5
Tobias Röhling <tobias.roehling@holisticon.de>
2023-09-21 13:05:22 +0200

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SelectComponent } from './select/select.component';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SelectComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgSelectModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/select/select.component.ts
@@ -0,0 +1,45 @@
+import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
+import { NgSelectComponent } from '@ng-select/ng-select';
+
+@Component({
+  selector: 'app-select',
+  templateUrl: './select.component.html',
+  styleUrls: ['./select.component.css']
+})
+export class SelectComponent {
+
+  items = [
+    { value: 1, label: 'Eins' },
+    { value: 2, label: 'Zwei' },
+    { value: 3, label: 'Drei' },
+    { value: 4, label: 'Vier' },
+  ];
+
+  @ViewChild(NgSelectComponent, { static: true })
+  ngSelect!: NgSelectComponent;
+
+  constructor(private cdRef: ChangeDetectorRef) {}
+
+  model: any = 2;
+
+  ngOnInit() {
+    /*setTimeout(() => {
+      console.log('Interval starting');
+      setInterval(() => {
+        this.model = getRandomInt(4) + 1;
+        console.log(`Changing value to ${this.model}`);
+        this.cdRef.markForCheck();
+      }, 2000);
+    }, 10000);*/
+  }
+
+  onClick(): void {
+    this.model = getRandomInt(4) + 1;
+    console.log(`Changing value to ${this.model}`);
+    // this.cdRef.markForCheck();
+  }
+}
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
